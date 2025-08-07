d64f6b2d988782619661588f2a65dc099297ae85
Venkat8161 <venkatramani1992@gmail.com>
2025-08-07 08:26:57 +0530

    Updated Pyautogui

--- RPA_Automation/pyautogui/pyautogui_demo.py
+++ RPA_Automation/pyautogui/pyautogui_demo.py
@@ -1,6 +1,6 @@
 import pyautogui
 import time
-"""
+
 #mouse operations
 #pyautogui.click(100,100)
 #time.sleep(2)
@@ -23,7 +23,7 @@ time.sleep(5)
 pyautogui.write("Python rpa_demo_1.py")
 
 pyautogui.press("enter")
-"""
+
 x,y = pyautogui.position()
 print(f'x: {x},y: {y}')
 pyautogui.moveTo(x,y,duration=1)
@@ -48,12 +48,12 @@ pyautogui.click()
 
 
 
-"""
+
 prices = [100, 250, 90]
 
 def discount(lst):
     return [p * 0.9 for p in lst if p > 100]
 
 print(discount(prices))
-"""
+
 
